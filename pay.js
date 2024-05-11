d5da2538d0f27f1f424a2c4b38c89756f4665df1
PeprahSamuel <146881591+PeprahSamuel@users.noreply.github.com>
2024-05-11 11:35:24 -0700

    Update pay.js

--- pay.js
+++ pay.js
@@ -4,7 +4,7 @@ function payWithPaystack(e) {
   e.preventDefault();
 
   let handler = PaystackPop.setup({
-    key: '<Replace with your Api Key>', // Replace with your public key
+    key: 'pk_test_d780b641742f72908086c4dcb19baa109dbfadd6', // Replace with your public key
     email: document.getElementById("email-address").value,
     amount: document.getElementById("amount").value * 100,
     currency: "GHS",
